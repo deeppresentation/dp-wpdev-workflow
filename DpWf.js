fdc1df776e6387a736f1fe41a8ee1cbc067af691
green-g <tomas.groulik@gmail.com>
2020-06-13 19:10:43 +0200

    Generic dp-wpdev-workflow commit 1592068240272

--- DpWf.js
+++ DpWf.js
@@ -1,13 +1,15 @@
-const fs = require('fs-extra');
+const fs  = require('fs-extra');
+const { readdirSync} = require('fs-extra');
 const path = require('upath');
 const notifier = require('node-notifier');
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 const ftp = require('vinyl-ftp');
-const ff = require('node-find-folder');
 const composer = require('gulp-composer');
 const dpWfHelper = require('./DpWfHelper');
+const simpleGit = require('simple-git');
 const dpLogo = 'DP-logo.png';
-
+const term = require('terminal-kit').terminal;
 
 
 class DpWf {
@@ -25,8 +27,12 @@ class DpWf {
         gulp.task('PROCESS:DIST_2_FTP', gulp.series(this.processDistDeployFtp.bind(this), this.notifyDist2Ftp.bind(this)));
         gulp.task('PROCESS:DIST_PACK_2_FTP', gulp.series(this.processDistDeployPackFtp.bind(this), this.notifyDistPack2Ftp.bind(this)));
 
-        gulp.task('UPDATE_G_MODULES', this.updateComposerGModules.bind(this));
-        gulp.task('PUSH_G_MODULES', this.pushComposerGModules.bind(this));
+        gulp.task('UPDATE_DP_MODULES', this.updateComposerDPModules.bind(this));
+        gulp.task('PUSH_DP_MODULES', this.pushComposerDPModules.bind(this));
+        gulp.task('PUSH_SELF', this.pushSelf.bind(this));
+        gulp.task('DEPLOY_2_WP_ORG', gulp.series(this.clearWordpressOrgTrunk.bind(this), this.deployPackFilesToWordpressOrg.bind(this)));
+
+        
     }
 
    _getPackageFilesAllBuilds(root) {
@@ -107,89 +113,104 @@ class DpWf {
         if (done) done();
     }
 
-    updateComposerGModules(done) {
-        var dirBkp = process.cwd();
-
-        var ff_result = new ff('vendor/greeng', {
-            nottraversal: ['dist', 'package']
+    updateComposerDPModules(done) {
+        composer('update deeppresentation/*');
+        notifier.notify({
+            title: '✅  UPDATED',
+            message: 'Deep presentation PHP modules was updated trough composer.',
+            icon: path.joinSafe(__dirname, dpLogo)
         });
-        try
-        {
-            ff_result.forEach((_item, _index, _array) => {
-                var greengRoot = path.join(dirBkp, _item);
-                composer('update greeng/*', {
-                    "working-dir": path.join(greengRoot, "..", "..")
-                });
-            });
-        }
-        catch (e){
-            console.log(e);
-            notifier.notify({
-                title: '❌  UPDATE FAILED',
-                message: `G modules update finished with error. Details: ${e}`,
-                icon: path.joinSafe(__dirname, dpLogo)
-            });
-        }
-        finally{
-            if (done) return done();   
-        }
-        
+        if (done) done();
     }
 
-    pushComposerGModules(done) {
-        var dirBkp = process.cwd();
-        var argv = require('yargs').argv;
-        var commitMessage = argv.m ? argv.m : `Generic library commit ${Date.now()}`;
+    getDirectories = source =>
+    {
+        return readdirSync(source, { withFileTypes: true })
+            .filter(dirent => dirent.isDirectory())
+            .map(dirent => path.joinSafe(source, dirent.name))
+    }
 
-        var ff_result = new ff('vendor/greeng', {
-            nottraversal: ['dist', 'package']
-        });
+    pushComposerDPModules(done) {
         try
         {
-        
-            ff_result.forEach((_item, _index, _array) => {
-                var greengRoot = path.join(dirBkp, _item);
-
-                var modules = fs.readdirSync(greengRoot, { withFileTypes: true })
-                    .filter(dirent => dirent.isDirectory())
-                    .map(dirent => dirent.name);
-                if (modules)
-                {
-                    modules.forEach((moduleName) => {
-                        var moduleDir = path.join(greengRoot, moduleName);
-                        console.log(moduleDir);
-
-                        var simpleGit = require('simple-git')(moduleDir);
-                        simpleGit
-                            .silent(false)
-                            .add('./*')
-                            .commit(commitMessage)
-                            .push('origin', 'master')
-                            .then(status => {
-                                notifier.notify({
-                                    title: '✅  PUSHED',
-                                    message: 'G module ' + moduleName +' was sucessfully commited and pushed into master. Details: ' + status,
-                                    icon: path.joinSafe(__dirname, dpLogo)
-                                });
-                            });
-                    });
-                }
-                return done();   
-
-            });
+            var dirBkp = process.cwd();
+            var argv = require('yargs').argv;
+            var commitMessage = argv.m ? argv.m : `Generic library commit ${Date.now()}`;
+            var modules = this.getDirectories(path.joinSafe(dirBkp, 'vendor', 'deeppresentation'));
+            this.pushGitModule( modules, 0, commitMessage, done);
         }
         catch (e){
             console.log(e);
             notifier.notify({
                 title: '❌  UPDATE FAILED',
-                message: `G modules update finished with error. Details: ${e}`,
+                message: `DP modules update finished with error. Details: ${e}`,
                 icon: path.joinSafe(__dirname, dpLogo)
             });
+            if (done) return done();
         }
-        finally{
-            if (done) return done();   
+    }
+
+    pushSelf(done) {
+        var argv = require('yargs').argv;
+        var dirBkp = process.cwd();
+        var commitMessage = argv.m ? argv.m : `Generic dp-wpdev-workflow commit ${Date.now()}`;
+        this.pushGitModule( [path.joinSafe(dirBkp,'dp-wpdev-workflow')], 0, commitMessage, done);  
+    }
+
+    pushGitModule( modules, idx, commitMessage, done){
+
+        if (idx < modules.length)
+        {
+            var moduleDir = modules[idx];
+            var git = simpleGit(moduleDir);
+            git.add('--all')
+                .then(() => git.commit(commitMessage))
+                .then(() => git.push('origin', 'master'))
+                .then(() => {
+                    term.green('DP module ' + moduleDir +' was sucessfully commited and pushed into master.\n');
+                    idx++;
+                    this.pushGitModule( modules, idx, commitMessage, done);
+                })
         }
-        
+        else if (done) done();
+    }
+
+    pullDPModules( modules, idx, commitMessage, done)
+    {
+        if (idx < modules.length)
+        {
+            var moduleDir = modules[idx];
+            composer('update deeppresentation/*'/*, {
+                "working-dir": path.join(greengRoot, "..", "..")
+            }*/);
+            var git = simpleGit(moduleDir);
+            git.add('--all')
+                .then(() => git.commit(commitMessage))
+                .then(() => git.push('origin', 'master'))
+                .then(() => {
+                    term.green('DP module ' + moduleDir +' was sucessfully commited and pushed into master.\n');
+                    idx++;
+                    this.pushGitModule( modules, idx, commitMessage, done);
+                })
+        }
+        else {
+            if (done) done();
+        }
+    }
+
+    clearWordpressOrgTrunk(done){
+        const wporgTrunkPath = path.joinSafe(this.config.wordpressOrgSvnBaseDir, this.config.id, 'trunk');
+        if (fs.existsSync(wporgTrunkPath))
+        {
+            return gulp.src(wporgTrunkPath)
+                .pipe(clean({force: true}));
+        }
+        else if (done) done();
+    }
+
+    deployPackFilesToWordpressOrg(){
+        return gulp.src(path.joinSafe(this.config.package.dir, this.config.id, '**', '*'))
+        .pipe(gulp.dest(path.joinSafe(this.config.wordpressOrgSvnBaseDir, this.config.id, 'trunk')));
     }
 }
 
--- to-copy-to-proj-root/gulpfile.js
+++ to-copy-to-proj-root/gulpfile.js
@@ -4,9 +4,12 @@ const gulp = require( 'gulp' );
 
 new DpWf(dpwfconfig);
 
-exports.default = gulp.series('CLEAR:FTP');
-exports.updategm = gulp.series('UPDATE_G_MODULES');
-exports.pushgm = gulp.series('PUSH_G_MODULES');
-//exports.deploy2git = gulp.series('PROCESS:DIST_2_GIT'); //will be covered in following releases
+exports.default =  gulp.series('CLEAR:FTP');
+exports.updatedpm = gulp.series('UPDATE_DP_MODULES');
+exports.pushdpm = gulp.series('PUSH_DP_MODULES');
+exports.pushself = gulp.series('PUSH_SELF');
+exports.deployPack = gulp.series('PROCESS:DIST_PACK_2_FTP');
+exports.deploy2git = gulp.series('PROCESS:DIST_2_GIT');
 exports.deploy2ftp = gulp.series('PROCESS:DIST_2_FTP');
 exports.clearftp = gulp.series('CLEAR:FTP');
+exports.deploy2wporg =  gulp.series('DEPLOY_2_WP_ORG');
